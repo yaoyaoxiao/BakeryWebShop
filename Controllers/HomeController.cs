05e759617911e5f71af630caf691e33fe653b773
yaoyaoxiao <30418688+yaoyaoxiao@users.noreply.github.com>
2018-04-19 19:10:09 -0700

    Add files via upload
    First Commit - Home, About, Menu, Order Online, Contact Us pages. Images not included.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Web;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BigPandaBakery.Models;
+
+namespace BigPandaBakery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Menu()
+        {
+            ViewData["Message"] = "The Menu page.";
+
+            return View();
+        }
+
+        public IActionResult Order()
+        {
+            ViewData["Message"] = "The Order Online page.";
+
+            return View();
+        }
+
+        public IActionResult ProductBread(int? id)
+        {
+            ViewData["Message"] = "The Product Bread page.";
+            return View();
+        }
+
+        public IActionResult ProductCake(int? id)
+        {
+            ViewData["Message"] = "The Product Cake page.";
+            //System.Web.HttpContext.Current.Session["ShowCart"] = "show";
+            return View();
+        }
+
+        public IActionResult ProductTea(int? id)
+        {
+            ViewData["Message"] = "The Product Tea page.";
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BigPandaBakery.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+
+        public Product()
+        {
+        }
+    }
+}
